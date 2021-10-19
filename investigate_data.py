f9a2c3acc058eaca78239c532551fa368dd189e4
Benjamin Rose <benjamin.rose@duke.edu>
2021-10-19 10:43:07 -0400

    Admin: move a few functions into utilities

--- investigate_data.py
+++ investigate_data.py
@@ -2,15 +2,11 @@
 
 Requires python 3.9
 """
-__version__ = "2021-09"
 
-from argparse import ArgumentParser, BooleanOptionalAction
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import numpy as np
 from numpy.polynomial import Polynomial
-from scipy.stats import binned_statistic, ks_2samp
 import seaborn as sns
 
 import linmix
@@ -23,37 +19,9 @@ from KS2D.KS2D import ks2d2s
 from fitres import Fitres
 from figures import *
 
+from util import parse_cli, bin_dataset, __version__
+
 sns.set_theme(context="talk", style="ticks", font="serif", color_codes=True)
-
-
-def bin_dataset(data, x, y, bins=25, error_stat=robust_scatter):
-    """
-    Parameters
-    -----------
-    data: pandas.DataFrame
-    x: str
-        column for x-axis (binning-axis)
-    y: str
-        column name for y-axis (statistic axis)
-    bins: int or sequence of scalars
-        passed to scipy.stats.binned_statistic. Defaults to 25 bins
-        (Not the same as scipy's default.)
-    stat: str, function
-        Passed to scipy.stats.binned_statistic. Defaults to
-        `br_util.stats.robust_scatter`.
-    """
-    SCATTER_FLOOR = 0.1
-    data_x_axis = data[x]
-    data_y_axis = data[y]
-
-    data_stat, data_edges, _ = binned_statistic(
-        data_x_axis, data_y_axis, statistic="median", bins=bins
-    )
-    data_error, _, _ = binned_statistic(
-        data_x_axis, data_y_axis, statistic=error_stat, bins=bins
-    )
-    data_error[data_error == 0] = SCATTER_FLOOR
-    return data_x_axis, data_y_axis, data_stat, data_edges, data_error
 
 
 def plot_binned(
@@ -277,43 +245,6 @@ def broken_beta(c, m_cosmo, m_red, M_0, c_break=0.3):
         [c <= c_break, c > c_break],
         [lambda x: m_cosmo * x + M_0, lambda x: m_red * x + M_0],
     )
-
-
-def parse_cli():
-    arg_parser = ArgumentParser(description=__doc__)
-    arg_parser.add_argument("--version", action="version", version=__version__)
-    arg_parser.add_argument(
-        "--bins",
-        type=int,
-        default=25,
-        help="number of bins to use in color-luminosity plot (default: %(default)s)",
-    )
-    arg_parser.add_argument(
-        "--cmax",
-        type=float,
-        default=2.0,
-        help="maximum c used in fitting BETA (default: %(default)s)",
-    )
-    arg_parser.add_argument(
-        "--linmix",
-        action=BooleanOptionalAction,
-        default=False,
-        help="run LINMIX to fit for BETA",
-    )  # python 3.9
-    arg_parser.add_argument(
-        "--alpha",
-        type=float,
-        default=0.15,
-        help="used for light-curve shape standardization (default: %(default)s)",
-    )
-    arg_parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        help="turn on verbose output (default: %(default)s)",
-    )
-
-    return arg_parser.parse_args()
 
 
 if __name__ == "__main__":
---
+++ util.py
@@ -0,0 +1,76 @@
+"""Utility functions"""
+
+__version__ = "2021-09"
+
+from argparse import ArgumentParser, BooleanOptionalAction
+
+from scipy.stats import binned_statistic
+
+from br_util.stats import robust_scatter
+
+
+def bin_dataset(data, x, y, bins=25, error_stat=robust_scatter):
+    """
+    Parameters
+    -----------
+    data: pandas.DataFrame
+    x: str
+        column for x-axis (binning-axis)
+    y: str
+        column name for y-axis (statistic axis)
+    bins: int or sequence of scalars
+        passed to scipy.stats.binned_statistic. Defaults to 25 bins
+        (Not the same as scipy's default.)
+    stat: str, function
+        Passed to scipy.stats.binned_statistic. Defaults to
+        `br_util.stats.robust_scatter`.
+    """
+    SCATTER_FLOOR = 0.1
+    data_x_axis = data[x]
+    data_y_axis = data[y]
+
+    data_stat, data_edges, _ = binned_statistic(
+        data_x_axis, data_y_axis, statistic="median", bins=bins
+    )
+    data_error, _, _ = binned_statistic(
+        data_x_axis, data_y_axis, statistic=error_stat, bins=bins
+    )
+    data_error[data_error == 0] = SCATTER_FLOOR
+    return data_x_axis, data_y_axis, data_stat, data_edges, data_error
+
+
+def parse_cli():
+    arg_parser = ArgumentParser(description=__doc__)
+    arg_parser.add_argument("--version", action="version", version=__version__)
+    arg_parser.add_argument(
+        "--bins",
+        type=int,
+        default=25,
+        help="number of bins to use in color-luminosity plot (default: %(default)s)",
+    )
+    arg_parser.add_argument(
+        "--cmax",
+        type=float,
+        default=2.0,
+        help="maximum c used in fitting BETA (default: %(default)s)",
+    )
+    arg_parser.add_argument(
+        "--linmix",
+        action=BooleanOptionalAction,
+        default=False,
+        help="run LINMIX to fit for BETA",
+    )  # python 3.9
+    arg_parser.add_argument(
+        "--alpha",
+        type=float,
+        default=0.15,
+        help="used for light-curve shape standardization (default: %(default)s)",
+    )
+    arg_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="turn on verbose output (default: %(default)s)",
+    )
+
+    return arg_parser.parse_args()
